0aa6f7bdcc03c54f0cfb3bfacc365d64865fe101
Abner Estévez <nertrabajo2@gmail.com>
2022-09-08 16:25:57 -0400

    JavaScript ready

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ let open1 = document.getElementById('open-modal');
 let close1 = document.getElementById('modal');
 let close2 = document.getElementById('close-modal');
 let mainModal = document.getAnimations('background-modal');
+let menu = document.getElementById('menuBars');
+let menuHeader = document.getElementById('header');
+let barsButton = document.getElementById('bars');
+let claseMain = document.getElementById('main');
+
+function barsMenu () {
+    if (menu.style.display != 'flex') {
+        menu.style.display = 'flex';
+        menuHeader.style.height = 'auto';
+        claseMain.style.margin = '140px 10% 0 10%';
+    } else if (menu.style.display = 'flex') {
+        menu.style.display = 'none';
+        menuHeader.style.height = '55px';
+        claseMain.style.margin = '70px 10% 0 10%';
+    }
+ }
 
 function modal () {
     close1.style.display = 'block';
@@ -19,3 +35,4 @@ function modalClose () {
 
 open1.addEventListener('click', modal);
 close2.addEventListener('click', modalClose);
+barsButton.addEventListener('click', barsMenu);
